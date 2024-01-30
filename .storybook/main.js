7a4f54594765954ed2b0d5614fdec3f15b9940f8
Edgarmejiav <edgarm_10@hotmail.com>
2024-01-30 08:56:32 -0500

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+  ],
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  features: {
+    interactionsDebugger: true,
+  },
+
+  docs: {
+    autodocs: true
+  }
+};
